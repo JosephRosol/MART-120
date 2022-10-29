9532fb778c13279c8967c7e3d9a2216d11710cd7
JosephRosol <joeswagger17@gmail.com>
2022-10-29 17:27:02 -0600

    A drawing of myself

---
+++ Homework 9/The_Photo_of_Joe_2022_10_29_01_58_14/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textSize(28);
+  text('Joe Swagger', 10, 30);
+  textSize(14);
+  text('By: Joseph Rosol', 275, 380);
+  triangle(30, 190, 88, 130, 86, 180);
+  circle(200, 200, 270);
+  circle(105, 150, 50);
+  rect(40,65,140,20);
+  square(180,-25,130);
+  point(88, 160);
+  point(88, 161);
+  line(67, 210, 90, 210);
+}
